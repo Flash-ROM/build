9936ce14e1f4076867177ac6a2394490ffc38d51
Surge1223 <surge1223@gmail.com>
2016-10-24 05:49:07 +0300

    main: Verbose makefile inclusion
    There are some cases where the verbose output is very useful. Make it configurable with the VERBOSE_MK_EVAL variable.
    
    @nathanchance edit: This is l33t enough to be turned on by default; people who don't like spam can export VERBOSE_MK_EVAL=false

--- core/main.mk
+++ core/main.mk
@@ -552,7 +552,17 @@ ifeq ($(USE_SOONG),true)
 subdir_makefiles := $(SOONG_ANDROID_MK) $(call filter-soong-makefiles,$(subdir_makefiles))
 endif
 
-$(foreach mk, $(subdir_makefiles), $(eval include $(mk)))
+ifneq ($(VERBOSE_MK_EVAL),false)
+  $(foreach mk, $(subdir_makefiles), \
+    $(info Including Makefile: $(mk)) \
+    $(eval include $(mk)) \
+    $(eval LOCAL_MODULE \:=$(mk)) \
+    $(eval LOCAL_DIR \:= $(patsubst %/,%,$(dir $(mk)))) \
+    $(info LOCAL_MODULE: $(LOCAL_MODULE)) \
+  )
+else
+  $(foreach mk, $(subdir_makefiles), $(eval include $(mk)))
+endif
 
 ifdef PDK_FUSION_PLATFORM_ZIP
 # Bring in the PDK platform.zip modules.

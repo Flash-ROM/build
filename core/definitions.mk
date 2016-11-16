a15f3de50a7b105711c1793fb770b24c69f8e880
Nathan Chancellor <natechancellor@gmail.com>
2016-11-16 09:08:01 -0700

    Core: print toolchain path during compilation
    I like to see what is being compiled with what so print it.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- core/definitions.mk
+++ core/definitions.mk
@@ -1205,7 +1205,7 @@ endef
 ###########################################################
 
 define transform-cpp-to-o
-@echo -e ${CL_GRN}"target $(PRIVATE_COMPILER_ID) $(PRIVATE_ARM_MODE) C++:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_GRN}"target $(PRIVATE_COMPILER_ID) $(PRIVATE_ARM_MODE) C++:"${CL_RST}" $(PRIVATE_CXX) | $(PRIVATE_MODULE) <= $<"
 @mkdir -p $(dir $@)
 $(hide) $(RELATIVE_PWD) $(PRIVATE_CXX) \
 	$(addprefix -I , $(PRIVATE_C_INCLUDES)) \
@@ -1258,7 +1258,7 @@ $(hide) $(RELATIVE_PWD) $(PRIVATE_CC) \
 endef
 
 define transform-c-to-o-no-deps
-@echo -e ${CL_GRN}"target $(PRIVATE_COMPILER_ID) $(PRIVATE_ARM_MODE) C:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_GRN}"target $(PRIVATE_COMPILER_ID) $(PRIVATE_ARM_MODE) C:"${CL_RST}" $(PRIVATE_CC) | $(PRIVATE_MODULE) <= $<"
 $(call transform-c-or-s-to-o-no-deps, \
     $(PRIVATE_CFLAGS) \
     $(PRIVATE_CONLYFLAGS) \
@@ -1312,7 +1312,7 @@ endef
 ###########################################################
 
 define transform-host-cpp-to-o
-@echo -e ${CL_YLW}"$($(PRIVATE_PREFIX)DISPLAY) $(PRIVATE_COMPILER_ID) C++:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_YLW}"$($(PRIVATE_PREFIX)DISPLAY) $(PRIVATE_COMPILER_ID) C++:"${CL_RST}" $(PRIVATE_CXX) | $(PRIVATE_MODULE) <= $<"
 @mkdir -p $(dir $@)
 $(hide) $(RELATIVE_PWD) $(PRIVATE_CXX) \
 	$(addprefix -I , $(PRIVATE_C_INCLUDES)) \
@@ -1363,7 +1363,7 @@ $(hide) $(RELATIVE_PWD) $(PRIVATE_CC) \
 endef
 
 define transform-host-c-to-o-no-deps
-@echo -e ${CL_YLW}"$($(PRIVATE_PREFIX)DISPLAY) $(PRIVATE_COMPILER_ID) C:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_YLW}"$($(PRIVATE_PREFIX)DISPLAY) $(PRIVATE_COMPILER_ID) C:"${CL_RST}" $(PRIVATE_CC) | $(PRIVATE_MODULE) <= $<"
 $(call transform-host-c-or-s-to-o-no-deps, $(PRIVATE_CFLAGS) $(PRIVATE_CONLYFLAGS) $(PRIVATE_DEBUG_CFLAGS))
 endef
 

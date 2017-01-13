587429d08a59417844203d7fd3a85b6c4687da70
Tao Bao <tbao@google.com>
2017-01-13 11:56:54 -0800

    releasetools: Delete the obsolete --aslr_mode/-a flag.
    It was added in commit 96be7205dce97977909e93b73f48779fcce3bc65
    ("Working ASLR implementation.") in 2010, and removed in commit
    1807e700a568d57901b835370d1b1ae05c3dbb0f ("don't generate retouch
    commands in OTA scripts") in 2012.
    
    Remove the obsolete --aslr_mode flag.
    
    Test: ota_from_target_files.py still works (by generating incremental
          and full OTAs respectively).
    
    Change-Id: I6d8e62730ac192f3574d484c4a4b9b43b4ee0a9e

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -80,9 +80,6 @@ Usage:  ota_from_target_files [flags] input_target_files output_ota_package
   -e  (--extra_script)  <file>
       Insert the contents of file at the end of the update script.
 
-  -a  (--aslr_mode)  <on|off>
-      Specify whether to turn on ASLR for the package (on by default).
-
   -2  (--two_step)
       Generate a 'two-step' OTA package, where recovery is updated
       first, so that any changes made to the system partition are done
@@ -158,7 +155,6 @@ OPTIONS.wipe_user_data = False
 OPTIONS.omit_prereq = False
 OPTIONS.downgrade = False
 OPTIONS.extra_script = None
-OPTIONS.aslr_mode = True
 OPTIONS.worker_threads = multiprocessing.cpu_count() // 2
 if OPTIONS.worker_threads == 0:
   OPTIONS.worker_threads = 1
@@ -1969,11 +1965,6 @@ def main(argv):
       OPTIONS.oem_no_mount = True
     elif o in ("-e", "--extra_script"):
       OPTIONS.extra_script = a
-    elif o in ("-a", "--aslr_mode"):
-      if a in ("on", "On", "true", "True", "yes", "Yes"):
-        OPTIONS.aslr_mode = True
-      else:
-        OPTIONS.aslr_mode = False
     elif o in ("-t", "--worker_threads"):
       if a.isdigit():
         OPTIONS.worker_threads = int(a)
@@ -2013,7 +2004,7 @@ def main(argv):
     return True
 
   args = common.ParseOptions(argv, __doc__,
-                             extra_opts="b:k:i:d:wne:t:a:2o:",
+                             extra_opts="b:k:i:d:wne:t:2o:",
                              extra_long_opts=[
                                  "board_config=",
                                  "package_key=",
@@ -2025,7 +2016,6 @@ def main(argv):
                                  "downgrade",
                                  "extra_script=",
                                  "worker_threads=",
-                                 "aslr_mode=",
                                  "two_step",
                                  "no_signing",
                                  "block",

832def18721318b0ce1d51b97f9f3ce1b478e1b5
Dan Willemsen <dwillemsen@google.com>
2017-02-18 10:41:30 -0800

    Add progress indicator to Android.mk includes
    Test: m -j
    Change-Id: I1e6fd020a06b49c243be14953d9f417e64ac7b8b

--- core/definitions.mk
+++ core/definitions.mk
@@ -2888,6 +2888,11 @@ $(strip $(if $(filter $(TARGET_ARCH),$(1)),$(TARGET_ARCH),\
   $(if $(filter $(TARGET_2ND_ARCH),$(1)),$(TARGET_2ND_ARCH),$(if $(1),none))))
 endef
 
+# $1 is the variable name to increment
+define inc_and_print
+$(strip $(eval $(1) := $($(1)) .)$(words $($(1))))
+endef
+
 ###########################################################
 ## Other includes
 ###########################################################
